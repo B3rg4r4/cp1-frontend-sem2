f0a047fee8bfc9a4964e4532f73bae61c715f2b8
B3rg4r4 <pedrobergara5@gmail.com>
2024-08-28 09:30:00 -0300

    boyler plate configurado pronto para iniciar o projeto

---
+++ cp1/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,13 @@
+export default function Cabecalho(){
+
+    return(
+
+        <header>
+            <div>
+                <h1> Cabecalho </h1>
+
+            </div>
+
+        </header>
+    )
+}
---
+++ cp1/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,13 @@
+export default function Rodape(){
+
+    return(
+
+        <footer>
+
+
+            
+        </footer>
+
+
+    )
+}
